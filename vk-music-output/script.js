5c989c70b34470c2b8a79b7286a6ed0043d3d3cb
Aɴᴅʀᴇᴡ <acimanx@gmail.com>
2018-07-06 03:49:19 +0200

    Update script.js

--- vk-music-output/script.js
+++ vk-music-output/script.js
@@ -7,15 +7,15 @@ obj.style.cssText = "position:fixed;top:1vh;right:1vw;width:20vw;height:50vh;bac
 var titleElement = document.getElementsByClassName("audio_row__performer_title");
 //search for title
 for (var i = 0; i < titleElement.length; i++) {
-//loop to show all titles
-var title = titleElement[i].innerText;
-var text = document.createTextNode(title);
-//show it as a text
-obj.appendChild(text);
-//attach text to output element
-var breakIt = document.createElement("br");
-obj.appendChild(breakIt);
-//attach br after text
+    //loop to show all titles
+    var title = titleElement[i].innerText;
+    var text = document.createTextNode(title);
+    //show it as a text
+    obj.appendChild(text);
+    //attach text to output element
+    var breakIt = document.createElement("br");
+    obj.appendChild(breakIt);
+    //attach br after text
 }
 
 document.body.appendChild(obj);
