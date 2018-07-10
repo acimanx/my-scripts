910ebad8058f2c77fecb001b04b9f455976ec216
Aɴᴅʀᴇᴡ <acimanx@gmail.com>
2018-07-10 14:58:26 +0200

    Update script.js

--- vk-music-output/script.js
+++ vk-music-output/script.js
@@ -2,8 +2,7 @@ const obj = document.createElement("div");
 obj.id = "output-it";
 obj.style.cssText = "position:fixed;top:1vh;right:1vw;width:20vw;height:50vh;background:#FFF;z-index:999;white-space:pre;overflow:auto";
 const titleElement = document.getElementsByClassName("audio_row__performer_title");
-let i = 0;
-for (i < titleElement.length; i++) {
+for (i = 0; i < titleElement.length; i++) {
     const toolMenuItem = titleElement[i].innerText;
     const text = document.createTextNode(toolMenuItem);
     obj.appendChild(text);
