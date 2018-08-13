877848649b57dc13050c83670ad1dcdf9fab824d
Aɴᴅʀᴇᴡ <acimanx@gmail.com>
2018-08-13 15:07:26 +0200

    Update script.js

--- vk-music-output/script.js
+++ vk-music-output/script.js
@@ -1,3 +1,4 @@
-const a = Array.from(document.getElementsByClassName("audio_row__performer_title")).map(({innerText}) => `${innerText}\n`).join("")
-const b = `<textarea style="position:fixed;top:0;right:0;width:50%;height:100%;background:#FFF;z-index:999;white-space:pre;overflow:auto">${a}\n</textarea>\n`;
-document.body.innerHTML += b;
+(() => {
+  const a = `<textarea style="position:fixed;top:0;right:0;width:50%;height:100%;background:#FFF;z-index:999;white-space:pre;overflow:auto">${Array.from(document.getElementsByClassName("audio_row__performer_title")).map(({innerText}) => `${innerText}\n`).join("")}\n</textarea>\n`;
+  document.body.innerHTML += a;
+})();
