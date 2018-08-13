a68c78b16df206a1af7158eed7609aafdc7888ea
Aɴᴅʀᴇᴡ <acimanx@gmail.com>
2018-08-13 15:01:01 +0200

    Update script.js

--- vk-music-output/script.js
+++ vk-music-output/script.js
@@ -1,9 +1,3 @@
-const titleElement = document.getElementsByClassName(
-  "audio_row__performer_title"
-);
-const arr = Array.from(titleElement);
-
-const markup =
-  `<textarea style="position:fixed;top:0;right:0;width:50%;height:100%;background:#FFF;z-index:999;white-space:pre;overflow:auto">${arr.map(({innerText}) => `${innerText}\n`).join('')}\n</textarea>\n`;
-
-document.body.innerHTML += markup
+const a = Array.from(document.getElementsByClassName("audio_row__performer_title")).map(({innerText}) => `${innerText}\n`).join("")
+const b = `<textarea style="position:fixed;top:0;right:0;width:50%;height:100%;background:#FFF;z-index:999;white-space:pre;overflow:auto">${a}\n</textarea>\n`;
+document.body.innerHTML += b;
